f3c73339fae6f7c4c5de9728854b837d04723126
Philip Samuelsson <112427706+PhilipSamuelsson@users.noreply.github.com>
2025-02-14 10:52:04 +0100

    Message yo

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)  # Allow external access
+    port = int(os.environ.get("PORT", 5000))  # Railway sätter PORT automatiskt
+    app.run(host="0.0.0.0", port=port)
